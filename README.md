1e1bf3cce8d12ed13f5348a11cd5dbf1dd476968
tiechui321 <487262091@qq.com>
2019-12-21 09:47:45 +0800

    add readme.md

---
+++ README.md
@@ -0,0 +1,10 @@
+## 码匠社区
+## 资料
+https://spring.io/guides
+https://spring.io/guides/gs/serving-web-content/
+https://elasticsearch.cn/
+https://git-scm.com/downloads
+
+
+
+## 工具
--- src/main/java/com/cx/community/controller/HelloController.java
+++ src/main/java/com/cx/community/controller/HelloController.java
@@ -15,6 +15,6 @@ public class HelloController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello";
+        return "index";
     }
 }
